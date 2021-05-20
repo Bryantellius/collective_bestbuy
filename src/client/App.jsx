c742d6004e94c83ff5e1e3fdd8be098f5b1ebcff
Steven Zimmerman <stevenpzimmerman@protonmail.com>
2021-05-20 11:12:36 -0500

    added navbar and probably broke some things

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Home from "./views/Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NavBar from "../../src/components/NavBar";
 
 const App = () => {
   return (
     <Router>
+      <div className = "navBar">
+      <NavBar />
+      </div>
       <Switch>
         <Route exact path="/">
           <Home />
---
+++ src/client/views/Sales.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Sales = () => {
+  const [test, setTest] = React.useState("");
+
+  React.useEffect(() => {
+    fetch("/api/test")
+      .then((res) => res.json())
+      .then(({ msg }) => setTest(msg));
+  }, []);
+
+  return (
+    <main className="container">
+      <h1>{test}</h1>
+      
+    </main>
+  );
+};
+
+export default Sales;
